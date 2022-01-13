ca8f6b30f0f7409df545e1d02b3fcda34a99449d
Ricardo Moreira <rgusto@gmail.com>
2022-01-12 22:46:18 -0300

    Criação do projeto 'Forum'

---
+++ forum/src/main/kotlin/br/com/alura/forum/model/Curso.kt
@@ -0,0 +1,9 @@
+package br.com.alura.forum.model
+
+data class Curso(
+
+    val id: Long? = null,
+    val nome: String,
+    val categoria: String
+
+)
---
+++ forum/src/main/kotlin/br/com/alura/forum/model/Resposta.kt
@@ -0,0 +1,12 @@
+package br.com.alura.forum.model
+
+import java.time.LocalDateTime
+
+data class Resposta(
+    val id: Long? = null,
+    val mensagem: String,
+    val dataCriacao: LocalDateTime = LocalDateTime.now(),
+    val autor: Usuario,
+    val topico: Topico,
+    val solucao: Boolean
+)
