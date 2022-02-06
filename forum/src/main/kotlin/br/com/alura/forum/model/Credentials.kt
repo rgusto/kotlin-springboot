3649543ae47b92445b3b31f1766d337d907f38ea
Ricardo Moreira <rgusto@gmail.com>
2022-02-06 11:12:56 -0300

    Segurança com JWT

---
+++ forum/src/main/kotlin/br/com/alura/forum/model/Credentials.kt
@@ -0,0 +1,8 @@
+package br.com.alura.forum.model
+
+data class Credentials (
+
+    val username: String = "",
+    val password: String = ""
+
+)
---
+++ forum/src/main/kotlin/br/com/alura/forum/security/JWTAuthenticationFilter.kt
@@ -0,0 +1,31 @@
+package br.com.alura.forum.security
+
+import br.com.alura.forum.config.JWTUtil
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class JWTAuthenticationFilter(private val jwtUtil: JWTUtil) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val token = request.getHeader("Authorization")
+        val jwt = getTokenDetail(token)
+        if (jwtUtil.isValid(token)) {
+            val authentication = jwtUtil.getAuthentication(jwt)
+            SecurityContextHolder.getContext().authentication = authentication
+        }
+    }
+
+    private fun getTokenDetail(token: String?): String? {
+        return token?.let { jwt ->
+            jwt.startsWith("Bearer ")
+            jwt.substring(7, jwt.length)
+        }
+    }
+}
